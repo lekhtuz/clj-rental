f632284d2ce61e91a6e48d8e539ebf5d8dbd55f3
lekhtuz <dlekhtuz@consumer.org>
2015-05-13 21:43:03 -0400

    added create-user test cases, fixed bugs

--- src/rental/schema.clj
+++ src/rental/schema.clj
@@ -62,7 +62,7 @@
 )
 
 (defn transact-data [data]
-  (log/info "transact-data: datae =" data)
+  (log/info "transact-data: data =" data)
   (try
     (let [future @(d/transact (conn) data)]
       (log/info "transact-data: future =" future)
@@ -120,32 +120,32 @@
           id (ffirst (d/q '[:find ?e :in $ ?u :where [?e ::username ?u]] (db) username))
           ent (d/entity (db) id)
         ]
-        (log/info "load-user: (class ent) =" (class ent) ", id =" id ", ent =" ent ", (keys ent) =" (keys ent))
+        (log/info "load-user: (class ent) =" (class ent) ", id =" id ", ent =" ent ", (keys ent) =" (keys ent) ", (::mailing-address ent) =" (::mailing-address ent))
         (if-not (nil? ent)
-          (merge {
-                  :id (:db/id ent)
-                  :usertype (-> ent ::usertype  usertype-role)
-                  :username (::username ent)
-                  :email (::email ent)
-                  :password (::password ent)
-                  :firstname (::first-name ent)
-                  :lastname (::last-name ent)
-                 }
-                 (let [ address (::mailing-address ent) ]
-                   (log/info "load-user: address =" address)
-                   (if-not (nil? address)
-                     (let [touched-address (d/touch address)]
-                       {
-                        :address-id (:db/id address)
-                        :address1 (:rental.schema.address/address1 address)
-                        :address2 (:rental.schema.address/address2 address)
-                        :city (:rental.schema.address/city address)
-                        :state (:rental.schema.address/state address)
-                        :zipcode (:rental.schema.address/zipcode address)
-                       }
-                     )
-                   )
-                 )
+          (merge 
+            (if-let [ address (::mailing-address ent) ]
+              (merge
+                (if-let [address2 (:rental.schema.address/address2 address)]
+                  { :address2 address2 }
+                )
+                {
+                 :address-id (:db/id address)
+                 :address1 (:rental.schema.address/address1 address)
+                 :city (:rental.schema.address/city address)
+                 :state (:rental.schema.address/state address)
+                 :zipcode (:rental.schema.address/zipcode address)
+                }
+              )
+            )
+            {
+             :id (:db/id ent)
+             :usertype (-> ent ::usertype  usertype-role)
+             :username (::username ent)
+             :email (::email ent)
+             :password (::password ent)
+             :firstname (::first-name ent)
+             :lastname (::last-name ent)
+            }
           )
         )
     )
@@ -154,30 +154,40 @@
 
 (defn create-user [params]
   (log/info "create-user: params =" params)
-  (let [{ :keys [ result ] :as transact-result } 
-        (transact-data
-          [
-           {
-            :db/id #db/id[:db.part/user]
-            ::usertype (-> params :usertype role-usertype)
-            ::username (:username params)
-            ::email (:email params)
-            ::password (creds/hash-bcrypt (:password params))
-            ::first-name (:firstname params)
-            ::last-name (:lastname params)
-            ::mailing-address {
-                               :rental.schema.address/address1 (:address1 params)
-                               :rental.schema.address/address2 (:address2 params)
-                               :rental.schema.address/city (:city params)
-                               :rental.schema.address/state (:state params)
-                               :rental.schema.address/zipcode (:zipcode params)
-                              }
-            }
-          ]
-        )
-       ]
-    (log/info "create-user: data transacted. transact-result =" transact-result)
-    result
+  (if (seq params)
+    (let [{ :keys [ result ] :as transact-result } 
+          (transact-data
+            [
+             (merge-with
+               merge 
+               {
+                :db/id #db/id[:db.part/user]
+                ::usertype (-> params :usertype role-usertype)
+                ::username (:username params)
+                ::email (:email params)
+                ::password (creds/hash-bcrypt (:password params))
+                ::first-name (:firstname params)
+                ::last-name (:lastname params)
+               }
+               (if (seq (:address1 params)) 
+                 { ::mailing-address {
+                                      :rental.schema.address/address1 (:address1 params)
+                                      :rental.schema.address/city (:city params)
+                                      :rental.schema.address/state (:state params)
+                                      :rental.schema.address/zipcode (:zipcode params)
+                                     }
+                 }
+               )
+               (if (seq (:address2 params))
+                 { ::mailing-address { :rental.schema.address/address2 (:address2 params) } }
+               )
+             )
+            ]
+          )
+         ]
+      (log/info "create-user: data transacted. transact-result =" transact-result)
+      result
+    )
   )
 )
 
--- test/rental/test/schema.clj
+++ test/rental/test/schema.clj
@@ -2,23 +2,40 @@
   (:use
     [clojure.test]
     [clojure.tools.logging :as log :refer [info]]
-    [rental.schema :refer [create-database delete-database load-user]]
+    [cemerick.friend.credentials :as creds]
+    [rental.schema :refer [create-database delete-database load-user create-user]]
   )
   (:import
 ;    [java.util Thread]
   )
 )
 
+(defn- same-user? [original copy]
+  (log/info "same-user?: original =" original)
+  (log/info "same-user?: copy =" copy)
+  (is (seq copy))
+  (is (= (:username copy) (:username original)))
+  (is (creds/bcrypt-verify (:password original) (:password copy)))
+  (is (= (:usertype copy) (:usertype original)))
+  (is (= (:firstname copy) (:firstname original)))
+  (is (= (:lastname copy) (:lastname original)))
+  (is (= (:address1 copy) (:address1 original)))
+  (is (= (:address2 copy) (:address2 original)))
+  (is (= (:city copy) (:city original)))
+  (is (= (:state copy) (:state original)))
+  (is (= (:zipcode copy) (:zipcode original)))
+)
+
 (deftest test-schema
   (testing "Testing delete database..."
-;           (is (delete-database))
+           (is (delete-database))
   )
 
   (log/info "Waiting 60 seconds for the database name to become available...")
-;  (. Thread sleep 60000)
+  (. Thread sleep 60000)
 
   (testing "Testing create database..."
-;           (is (create-database))
+           (is (create-database))
   )
 
   (testing "Testing load-user..."
@@ -68,5 +85,51 @@
   )
 
   (testing "Testing create-user..."
+           (is (nil? (create-user nil)))
+
+           (let [params {
+                         :username "test-admin"
+                         :password "password"
+                         :usertype :rental.auth/role-admin
+                         :firstname "Fadmin"
+                         :lastname "Ladmin"
+                         :email "test-admin@email.com"
+                        }]
+             (is (create-user params))
+             (same-user? params (load-user (:username params)))
+           )
+
+           (let [params {
+                         :username "test-landlord"
+                         :password "password"
+                         :usertype :rental.auth/role-landlord
+                         :firstname "Flandlord"
+                         :lastname "Llandlord"
+                         :email "test-landlord@email.com"
+                         :address1 "1 Main St"
+                         :city "Franklin Lakes"
+                         :state "NJ"
+                         :zipcode "07417"
+                        }]
+             (is (create-user params))
+             (same-user? params (load-user (:username params)))
+           )
+
+           (let [params {
+                         :username "test-tenant"
+                         :password "password"
+                         :usertype :rental.auth/role-tenant
+                         :firstname "Ftenant"
+                         :lastname "Ltenant"
+                         :email "test-tenant@email.com"
+                         :address1 "1 Mountain View Dr"
+                         :address2 "right hand side of the street"
+                         :city "Woodland Park"
+                         :state "NJ"
+                         :zipcode "07424"
+                        }]
+             (is (create-user params))
+             (same-user? params (load-user (:username params)))
+           )
   )
 )
